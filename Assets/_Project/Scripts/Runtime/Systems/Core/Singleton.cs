51a87fd6962378fd2f84a817405e6fd860171849
ErickTorres <erickcanvas@gmail.com>
2023-09-01 22:53:17 -0300

    Added singleton core

---
+++ Assets/_Project/Scripts/Runtime/Systems/Core/Singleton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace DreamTeam.Runtime.System.Core
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance;
+
+        public bool dontDestroy = true;
+
+        protected virtual void Awake()
+        {
+            if (Instance != null)
+            {
+                string typename = typeof(T).Name;
+                Debug.LogWarning($"More that one instance of {typename} found.");
+                Debug.LogError("Clean your scene!");
+                Destroy(gameObject);
+            }
+
+            Instance = this as T;
+
+            if (dontDestroy)
+                DontDestroyOnLoad(gameObject);
+        }
+    }
+}
