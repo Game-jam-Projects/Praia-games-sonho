ae4c20ebd2e98621fdbd3747cd3e20d78cacbc2a
ErickTorres <erickcanvas@gmail.com>
2023-09-02 01:16:43 -0300

    Added health system

---
+++ Assets/_Project/Scripts/Runtime/Systems/HealthSystem/DetectHealthSystem.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class DetectHealthSystem : MonoBehaviour
+{
+    public enum DetectionType
+    {
+        Collision,
+        Trigger,
+    }
+
+    [SerializeField] private DetectionType type;
+    [Tooltip("Quantidade de dano")]
+    [SerializeField] private float damage;
+    [Tooltip("Quantidade de cura")]
+    [SerializeField] private float heal;
+
+    [SerializeField] private bool destroyOnCollide = true;
+    
+    [SerializeField] private GameObject spawnEffectPrefab;
+
+    private void Start()
+    {
+        //teste de mudanças de dano
+       //damage = PowerUpController.Instance.m_player.BulletDamage;
+      
+
+    }
+
+  
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (type != DetectionType.Collision)
+            return;
+
+        if(collision.gameObject.TryGetComponent(out IDamageable damageable))
+        {
+            damageable.TakeDamage(transform.position, damage);
+            damageable.Heal(heal);
+        }
+
+        DestroyObject();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (type != DetectionType.Trigger)
+            return;
+
+        if (collision.gameObject.TryGetComponent(out IDamageable damageable))
+        {
+            damageable.TakeDamage(transform.position, damage);
+            damageable.Heal(heal);
+        }
+
+        DestroyObject();
+    }
+
+    public void SetChangeDamage(float valor)
+    {
+        damage += valor;
+    }
+
+    private void DestroyObject()
+    {
+        if (!destroyOnCollide)
+            return;
+
+        if (spawnEffectPrefab)
+            Instantiate(spawnEffectPrefab, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_Project/Scripts/Runtime/Systems/HealthSystem/HealthSystem.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour, IDamageable
+{
+    [field: SerializeField] public float MaxHealth { get; set; }
+
+    public float CurrentHealth { get; set; }
+    public bool IsDie { get ; set ; }
+
+    public event Action<HealthArgs> OnChangeHealth;
+    public event Action<IDamageable> OnDie;
+    public event Action<Vector3> OnTakeDamage;
+    public event Action OnHeal;
+
+    [SerializeField] private bool destroyOnDie;
+    [SerializeField] private bool disableOnDie;
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+        OnChangeHealth?.Invoke(new() { current = CurrentHealth, max = MaxHealth });
+    }
+
+    public void TakeDamage(Vector3 direction, float damage)
+    {
+        if (damage <= 0)
+            return;
+
+        CurrentHealth -= damage;
+
+        if (CurrentHealth < 0)
+        {
+            Die();
+            return;
+        }
+
+        OnChangeHealth?.Invoke(new() { current = CurrentHealth, max = MaxHealth });
+        OnTakeDamage?.Invoke(direction);
+    }
+
+    public void Die()
+    {
+        if (IsDie)
+            return;
+        IsDie = true;
+        OnDie?.Invoke(this);
+
+
+        if (destroyOnDie)
+            Destroy(gameObject);
+
+        else if(disableOnDie)
+            gameObject.SetActive(false);
+    }
+
+    public void Heal(float amount)
+    {
+        if (amount <= 0)
+            return;
+
+        CurrentHealth += amount;
+
+        if (CurrentHealth > MaxHealth)
+            CurrentHealth = MaxHealth;
+
+        OnChangeHealth?.Invoke(new() { current = CurrentHealth, max = MaxHealth });
+        OnHeal?.Invoke();
+    }
+}
