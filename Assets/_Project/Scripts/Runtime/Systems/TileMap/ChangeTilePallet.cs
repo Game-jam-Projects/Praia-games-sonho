bd73466da3878703cb8059e05f8076893ebb5159
pwmuniz <aprendaunity@gmail.com>
2023-09-02 14:50:00 -0300

    Implementação do sistema de Troca do TilePallet

---
+++ Assets/_Project/Scripts/Runtime/Systems/TileMap/ChangeTilePallet.cs
@@ -0,0 +1,49 @@
+using DreamTeam.Runtime.Systems.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ChangeTilePallet : MonoBehaviour
+{
+    public TileBase[] dream;
+    public TileBase[] nightmare;
+
+    public Tilemap[] Tilemaps;
+
+    private void Start()
+    {
+        CoreSingleton.Instance.gameStateManager.ChagedStageType += EChangeStageType;
+        Tilemaps = GetComponentsInChildren<Tilemap>();
+    }
+
+    public void EChangeStageType(StageType stageType)
+    {
+        switch (stageType)
+        {
+            case StageType.Dream:
+
+                foreach (Tilemap map in Tilemaps)
+                {
+                    for (int i = 0; i < nightmare.Length; i++)
+                    {
+                        map.SwapTile(nightmare[i], dream[i]);
+                    }
+                }
+
+                break;
+
+            case StageType.Nightmare:
+
+                foreach (Tilemap map in Tilemaps)
+                {
+                    for (int i = 0; i < dream.Length; i++)
+                    {
+                        map.SwapTile(dream[i], nightmare[i]);
+                    }
+                }
+
+                break;
+        }
+    }
+}
