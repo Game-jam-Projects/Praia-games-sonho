365ae1b743c748ca2636186d74641c9424745bff
ErickTorres <erickcanvas@gmail.com>
2023-09-03 22:33:12 -0300

    Added pass gameobjects

---
+++ Assets/_Project/Scripts/Runtime/Utilities/PassGameObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace DreamTeam.Runtime.Utilities
+{
+    public class PassGameObject : MonoBehaviour
+    {
+        public event Action OnFinishedCutscene;
+
+        [SerializeField] private GameObject[] gameObjects;
+        private int index;
+        private bool finished;
+
+        private void Start()
+        {
+            UpdateHandle();
+        }
+
+        public void Next()
+        {
+            index++;
+
+            if (index == gameObjects.Length && finished == false)
+            {
+                index = gameObjects.Length;
+
+                finished = true;
+
+                OnFinishedCutscene?.Invoke();
+                return;
+            }
+
+            UpdateHandle();
+        }
+
+        public void UpdateHandle()
+        {
+            foreach (GameObject go in gameObjects)
+            {
+                go.SetActive(false);
+            }
+
+            gameObjects[index].SetActive(true);
+        }
+    }
+}
