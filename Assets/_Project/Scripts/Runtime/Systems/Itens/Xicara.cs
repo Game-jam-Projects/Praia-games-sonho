d1375a3c3853344c8d1d35e2583b9c16aa987673
ErickTorres <erickcanvas@gmail.com>
2023-09-04 13:44:31 -0300

    Added xicara

--- Assets/_Project/Scripts/Runtime/Systems/Itens/Xicara.cs
+++ Assets/_Project/Scripts/Runtime/Systems/Itens/Xicara.cs
@@ -7,6 +7,7 @@ public class Xicara : MonoBehaviour
     {
         if (collision.TryGetComponent(out PlayerController controller))
         {
+            controller.GetInput().DisableInput();
             SceneLoader.Instance.NextScene();
         }
     }
