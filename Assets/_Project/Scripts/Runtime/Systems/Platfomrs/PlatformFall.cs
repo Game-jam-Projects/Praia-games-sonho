85e148952e79fbc730dcf5fb9029fa29e1613826
pwmuniz <aprendaunity@gmail.com>
2023-09-04 17:59:46 -0300

    atualuzações

--- Assets/_Project/Scripts/Runtime/Systems/Platfomrs/PlatformFall.cs
+++ Assets/_Project/Scripts/Runtime/Systems/Platfomrs/PlatformFall.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DreamTeam.Runtime.Systems.Core;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlatformFall : MonoBehaviour
@@ -11,14 +12,22 @@ public class PlatformFall : MonoBehaviour
     [SerializeField] private Animator anim;
     private Rigidbody2D rbPlatform;
     private bool isFalling;
-
+    private Vector3 startPosition;
+    [SerializeField] private float fallTime;
 
     // Start is called before the first frame update
     void Start()
     {
+        startPosition = transform.position;
         rbPlatform = GetComponent<Rigidbody2D>();
+        CoreSingleton.Instance.gameManager.OnTransitionFinished += ResetPlataform;
     }
 
+
+    private void OnDisable()
+    {
+        CoreSingleton.Instance.gameManager.OnTransitionFinished -= ResetPlataform;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -42,6 +51,21 @@ public class PlatformFall : MonoBehaviour
         anim.enabled = false;
         anim.SetTrigger("ItFell");
         rbPlatform.bodyType = RigidbodyType2D.Dynamic;
+        StartCoroutine(nameof(IEFallController));
 
+    }
+
+    private IEnumerator IEFallController()
+    {
+        yield return new WaitForSeconds(fallTime);
+        rbPlatform.velocity = Vector2.zero;
+        rbPlatform.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    private void ResetPlataform()
+    {
+        isFalling = false;
+        rbPlatform.bodyType = RigidbodyType2D.Kinematic;
+        transform.position = startPosition;
     }
 }
